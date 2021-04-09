bcd06301e4fc0ac43f8f930fe4185ca3a2158598
Dinara Shabanova <dinara_shabanova@epam.com>
2021-04-09 12:21:59 +0300

    tests for ios apps

---
+++ src/main/java/nativeapp/ios/photos/PhotosApp.java
@@ -0,0 +1,17 @@
+package nativeapp.ios.photos;
+
+import com.epam.jdi.light.mobile.elements.common.app.Button;
+import com.epam.jdi.light.mobile.elements.pageobjects.annotations.JApp;
+import com.epam.jdi.light.mobile.elements.pageobjects.annotations.MobileFindBy;
+
+import static com.epam.jdi.light.driver.WebDriverFactory.getDriver;
+
+@JApp(app = "com.apple.mobileslideshow")
+public class PhotosApp {
+
+    public static PhotosPage photosPage;
+
+    @MobileFindBy(xpath = "//XCUIElementTypeButton[@name=\"Continue\"]")
+    public static Button continueButton;
+
+}
